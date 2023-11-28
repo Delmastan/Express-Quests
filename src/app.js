db6960db563c3b550d7930060699c421d37acdf5
DELMAS <delmastristan64@gmail.com>
2023-11-28 12:36:19 +0100

    finish challenge express_3

--- src/app.js
+++ src/app.js
@@ -7,12 +7,15 @@ app.use(express.json());
 const movieControllers = require("./controllers/movieControllers");
 const userControllers = require("./controllers/userControllers");
 
-app.post("/api/movies", movieControllers.postMovie);
-app.post("/api/users", userControllers.postUser);
-
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 app.get("/api/users", userControllers.getUsers);
 app.get("/api/users/:id", userControllers.getUserById);
+
+app.post("/api/movies", movieControllers.postMovie);
+app.post("/api/users", userControllers.postUser);
+
+app.put("/api/movies/:id", movieControllers.updateMovie);
+app.put("/api/users/:id", userControllers.updateUser);
 
 module.exports = app;
