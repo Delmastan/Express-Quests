dfbb6a9e0fd911a4adacf0d7b2b725f79155790c
DELMAS <delmastristan64@gmail.com>
2023-11-29 16:59:52 +0100

    finished ecpress_3.2

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ const app = express();
 
 app.use(express.json());
 
+const validateMovie = require("./middlewares/validateMovie");
+const validateUser = require("./middlewares/validateUser");
+
 const movieControllers = require("./controllers/movieControllers");
 const userControllers = require("./controllers/userControllers");
 
@@ -12,10 +15,14 @@ app.get("/api/movies/:id", movieControllers.getMovieById);
 app.get("/api/users", userControllers.getUsers);
 app.get("/api/users/:id", userControllers.getUserById);
 
-app.post("/api/movies", movieControllers.postMovie);
-app.post("/api/users", userControllers.postUser);
+// app.post("/api/movies", movieControllers.postMovie);
+// app.post("/api/users", userControllers.postUser);
+app.post("/api/movies", validateMovie, movieControllers.postMovie);
+app.post("/api/users", validateUser, userControllers.postUser);
 
-app.put("/api/movies/:id", movieControllers.updateMovie);
-app.put("/api/users/:id", userControllers.updateUser);
+// app.put("/api/movies/:id", movieControllers.updateMovie);
+// app.put("/api/users/:id", userControllers.updateUser);
+app.put("/api/movies/:id", validateMovie, movieControllers.updateMovie);
+app.put("/api/users/:id", validateUser, userControllers.updateUser);
 
 module.exports = app;
